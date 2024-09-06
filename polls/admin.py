8e1a3edcddb26cf97e2c101f09718316d15bd75e
tribalchiefjj <153628222+tribalchiefjj@users.noreply.github.com>
2024-09-06 19:21:26 +0300

    Revert "Revert "added new files""

--- polls/admin.py
+++ polls/admin.py
@@ -3,3 +3,5 @@ from django.contrib import admin
 from .models import Question
 
 admin.site.register(Question)
+
+# Register your models here.
--- polls/models.py
+++ polls/models.py
@@ -2,21 +2,24 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-
+    
     def __str__(self):
-        return self.question_text
+        return self.question_text  # Returns the question text as the string representation
+    
+    # Custom method to check if the question was published within the last 24 hours
+        def was_published_recently(self):
+            return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-
+    
     def __str__(self):
-        return self.choice_text
+        return self.choice_text  # Returns the choice text as the string representation
+# Create your models here.
